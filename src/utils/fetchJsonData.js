2a9b9826bc491505751562692673cc467554c118
ndejoma <ndegwaj@proton.me>
2023-03-02 12:15:27 +0300

    Add pre-commit hook to format code

--- src/utils/fetchJsonData.js
+++ src/utils/fetchJsonData.js
@@ -4,7 +4,7 @@ export default async function fetchJsonData(...args) {
     const jsonData = await res.json();
     return jsonData;
   } else {
-    const errData = await res?.json();
+    const errData = await res.json();
     throw new Error({
       statusCode: res?.status,
       data: errData,
